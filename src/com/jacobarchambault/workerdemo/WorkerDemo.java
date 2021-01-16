dcc32de47a01f3be93b7ec5800fde55bab1d08f6
Jacob Archambault <jacobarchambault@gmail.com>
2021-01-16 01:06:07 -0500

    Remove named variables from WorkerDemo

--- src/com/jacobarchambault/workerdemo/WorkerDemo.java
+++ src/com/jacobarchambault/workerdemo/WorkerDemo.java
@@ -5,16 +5,14 @@ public class WorkerDemo {
 	public static void main(String[] args) {
 		// Create a ProductionWorker object and pass the initialization
 		// data to the constructor.
-		TeamLeader tl = new TeamLeader("John Smith", "123-A", "11-15-2005", ProductionWorker.DAY_SHIFT, 16.50, 500.00,
-				5.0, 2.5);
-
+		new TeamLeader("John Smith", "123-A", "11-15-2005", ProductionWorker.DAY_SHIFT, 16.50, 500.00,
+				5.0, 2.5)
 		// Display the data.
-		tl.info("Here's the first team leader.");
+		.info("Here's the first team leader.");
 
 		// Create another ProductionWorker object.
-		TeamLeader pw2 = new TeamLeader("Joan Jones", "222-L", "12-12-2005", ProductionWorker.NIGHT_SHIFT, 18.50, 10000,
-				30, 10);
-
-		pw2.info("\nHere's the second team leader.");
+		new TeamLeader("Joan Jones", "222-L", "12-12-2005", ProductionWorker.NIGHT_SHIFT, 18.50, 10000,
+				30, 10)
+		.info("\nHere's the second team leader.");
 	}
 }
